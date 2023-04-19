abe49cec8e388d27e1aa3165dce0536e1c52b97f
MaximShaybakov <momon_boton@mail.ru>
2023-04-20 02:04:04 +0500

    Create models.py
    User model.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,42 @@
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from manager import UserManager
 
 
-class UserManager(BaseUserManager):
-    pass
+class User(AbstractUser):
+    """Standard user. """
+    USER_STATUS_CHOICES: tuple = (
+        ('stud', 'student'),
+        ('teach', 'teacher'),
+        ('parent', 'parent'),
+        ('oth', 'other'),
+    )
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email'), unique=True)
+    status = models.CharField(_('user_type'),
+                              choices=USER_STATUS_CHOICES,
+                              max_length=6),
+    username = models.CharField(_('username'),
+                                max_length=50,
+                                error_messages={'unique': _("A user with that username already exists.")},
+                                help_text='No more than 50 characters',
+                                validators=UnicodeUsernameValidator())
+
+    def __str__(self) -> str:
+        return f'{self.first_name}, id = {self.pk}'
+
+    def __repr__(self) -> str:
+        return f'{super.__name__}'
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = "Users list"
+        ordering = ('email',)
