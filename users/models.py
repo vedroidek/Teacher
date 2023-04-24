368192e1cd45c44ef558361ac56fe0b7554d8956
MaximShaybakov <momon_boton@mail.ru>
2023-04-24 13:06:24 +0500

    Fix models.py for User
    Redesigned custom user model to increase flexibility

--- users/models.py
+++ users/models.py
@@ -1,42 +1,73 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from .manager import UserManager
+from .manager import CustomUserManager
 
 
-class User(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin, CustomUserManager):
     """Standard user. """
-    USER_STATUS_CHOICES: tuple = (
+    USER_STATUS_CHOICES = [
         ('stud', 'student'),
         ('teach', 'teacher'),
         ('parent', 'parent'),
-        ('oth', 'other'),
-    )
-    REQUIRED_FIELDS = []
-    objects = UserManager()
-    USERNAME_FIELD = 'email'
+        ('oth', 'other')
+    ]
+
+    GENDER_CHOICES = [
+        ('f', 'female'),
+        ('m', 'male')
+    ]
+
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'username'
     email = models.EmailField(_('email'), unique=True)
-    status = models.CharField(_('user_type'),
-                              choices=USER_STATUS_CHOICES,
-                              max_length=6),
     username = models.CharField(_('username'),
-                                max_length=50,
+                                unique=True,
+                                max_length=150,
+                                validators=[UnicodeUsernameValidator],
                                 error_messages={'unique': _("A user with that username already exists.")},
-                                help_text='No more than 50 characters')
-    username_validator = UnicodeUsernameValidator()
+                                help_text='At least 3 chars and no more than 150 chars')
+    first_name = models.CharField(_('first_name'),
+                                  max_length=150,
+                                  blank=True)
+    last_name = models.CharField(_('last_name'),
+                                 max_length=150,
+                                 blank=True)
+    user_type = models.CharField(_('user_type'),
+                                 choices=USER_STATUS_CHOICES,
+                                 max_length=6,
+                                 blank=False,
+                                 null=True),
+    gender = models.CharField(_('gender'),
+                              max_length=1,
+                              choices=GENDER_CHOICES,
+                              help_text=r"'f'emale or 'm'ale",
+                              blank=False, null=False)
+    is_admin = models.BooleanField(_('is_admin'),
+                                   default=False,
+                                   editable=False)
+    created_at = models.DateTimeField(_('created_at'),
+                                      auto_now=True)
+
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
-        return f'{self.first_name}, id = {self.pk}'
+        return f'{self.username}'
 
     def __repr__(self) -> str:
-        return f'{super.__name__}'
+        return f'{self.__class__.__name__}: {self.username} - pk:{self.pk}'
 
     @property
     def full_name(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        if all([self.first_name, self.last_name]):
+            return f'{self.first_name} {self.last_name}'
+        else:
+            return f'{self.username}'
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = "Users list"
-        ordering = ('email',)
+        ordering = ['username', 'email']
+        db_table = 'users'
