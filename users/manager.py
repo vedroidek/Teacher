077b8422b28e5aa3c28cf12df3ac2f9b94ae2b4c
MaximShaybakov <momon_boton@mail.ru>
2023-04-24 13:03:55 +0500

    Change manager.py
    Added classmethod for correctly name if username is not exist

--- users/manager.py
+++ users/manager.py
@@ -1,33 +1,37 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.hashers import make_password
 
 
-class UserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email: str, password: str, **extra_fields):
-        """Create user.
+    def _create_user(self, email: str, password: str, username: str = None, **extra_fields):
+        """Management user.
         Required fields: email, password.
         """
         if not email:
-            raise ValueError('e-mail cannot be empty')
+            raise ValueError('email cannot be empty')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.password = make_password(password)
+
+        if not username:
+            username = self.name_from_email(email=email)
+
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email: str, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+    @classmethod
+    def name_from_email(cls, email: str):
+        name, domain = email.strip().rsplit("@", 1)
+        return name
 
-    def create_superuser(self, email: str, password: str, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_user(self, email: str, password: str, username: str, **extra_fields):
+        extra_fields.setdefault('is_admin', False)
+        return self._create_user(email, password, username, **extra_fields)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, email: str, password: str, username: str, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must have is_admin=True.')
+        return self._create_user(email, password, username, **extra_fields)
