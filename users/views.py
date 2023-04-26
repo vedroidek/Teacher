401edde4f6bb3838b89adaa4c71f29e2c599ab7a
MaximShaybakov <momon_boton@mail.ru>
2023-04-26 16:49:09 +0500

    Create view for user_add form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django import forms
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.views.generic import FormView
+from rest_framework import status
+from rest_framework.response import Response
+
+from users.forms import CustomUserCreationForm
+
+
+class UserFormView(FormView):
+    template_name = 'users/register_form.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+    
+    def form_valid(self, form):
+        get_user_model().objects.create(**form.cleaned_data)
+        return super().form_valid(form)
