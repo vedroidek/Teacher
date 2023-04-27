7181445b35d6de38d4b9287879bca3539d80e4d8
MaximShaybakov <momon_boton@mail.ru>
2023-04-28 01:32:25 +0500

    Change settings.py (PASSWORD_HASHERS)
    BCrypt is set by default to hash passwords.

--- teacher_site/settings.py
+++ teacher_site/settings.py
@@ -90,6 +90,14 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
